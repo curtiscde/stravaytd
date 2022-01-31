5d435ee2db36466342ab09be0b536dc5d6f757a0
Curtis Timson <curt@live.co.uk>
2022-01-31 12:47:34 +0000

    add logs

--- gh-action-scripts/storeActivity.js
+++ gh-action-scripts/storeActivity.js
@@ -5,6 +5,14 @@ const storeActivity = async () => {
   const distance = Number(process.env.npm_config_distance);
   const movingTime = Number(process.env.npm_config_movingtime);
 
+  console.log('process.env.npm_config_activityid', process.env.npm_config_activityid)
+  console.log('process.env.npm_config_distance', process.env.npm_config_distance)
+  console.log('process.env.npm_config_movingtime', process.env.npm_config_movingtime)
+
+  console.log('activityId', activityId)
+  console.log('distance', distance)
+  console.log('movingTime', movingTime)
+
   await fs.writeFile(`../pending-activities/activity${activityId}.json`, JSON.stringify({
     activityId, distance, movingTime
   }));
