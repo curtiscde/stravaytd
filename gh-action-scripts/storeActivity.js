d8275807e59f624158204b8dd70d33eb650e3667
Curtis Timson <curt@live.co.uk>
2022-01-31 17:22:20 +0000

    create dir if it does not exist

--- gh-action-scripts/storeActivity.js
+++ gh-action-scripts/storeActivity.js
@@ -1,19 +1,20 @@
-import fs from 'fs/promises';
+import fs from 'fs';
+import fsp from 'fs/promises';
 
 const storeActivity = async () => {
   const activityId = Number(process.env.npm_config_activityid);
   const distance = Number(process.env.npm_config_distance);
   const movingTime = Number(process.env.npm_config_movingtime);
 
-  console.log('process.env.npm_config_activityid', process.env.npm_config_activityid)
-  console.log('process.env.npm_config_distance', process.env.npm_config_distance)
-  console.log('process.env.npm_config_movingtime', process.env.npm_config_movingtime)
-
   console.log('activityId', activityId)
   console.log('distance', distance)
   console.log('movingTime', movingTime)
 
-  await fs.writeFile(`../pending-activities/activity${activityId}.json`, JSON.stringify({
+  const dir = '../pending-activities';
+
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+  await fsp.writeFile(`${dir}/activity${activityId}.json`, JSON.stringify({
     activityId, distance, movingTime
   }));
 }
