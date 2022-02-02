eb9d3519e126b774cefdd2be210d8097fe4ad751
Curtis Timson <curt@live.co.uk>
2022-02-02 19:14:32 +0000

    add typescript

---
+++ gh-action-scripts/build/tt.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tt2_1 = require("./util/tt2");
+function tt() {
+    var g = (0, tt2_1.tt2)('testing');
+    console.log(g);
+}
+tt();
---
+++ gh-action-scripts/build/util/tt2.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.tt2 = void 0;
+function tt2(p) {
+    return "".concat(p, "890");
+}
+exports.tt2 = tt2;
