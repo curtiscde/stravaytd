bddefce2475c8858b857ff49e9633c969edade12
Curtis Timson <curt@live.co.uk>
2022-02-02 19:16:44 +0000

    delete build folder

--- gh-action-scripts/build/tt.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tt2_1 = require("./util/tt2");
-function tt() {
-    var g = (0, tt2_1.tt2)('testing');
-    console.log(g);
-}
-tt();
--- gh-action-scripts/build/util/tt2.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tt2 = void 0;
-function tt2(p) {
-    return "".concat(p, "890");
-}
-exports.tt2 = tt2;
