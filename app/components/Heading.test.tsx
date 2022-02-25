af2414a05ca7b12ab48c8298c6bee4afc117b25c
Curtis Timson <curt@live.co.uk>
2022-02-25 20:26:40 +0000

    ✅ Heading.tsx test (#29)

---
+++ app/components/Heading.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, screen } from '@testing-library/react'
+import Heading from "./Heading";
+
+describe('Header', () => {
+  beforeEach(() => {
+    render(<Heading title={'test title'} subtitle={'test subtitle'} />)
+  });
+
+  it('displays title', () => {
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: 'test title',
+    })).toBeTruthy()
+  })
+
+  it('displays subtitle', () => {
+    expect(screen.getByText('test subtitle')).toBeTruthy()
+  })
+})
