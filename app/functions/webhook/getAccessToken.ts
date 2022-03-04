db3965fed6da592965b48b48a27834ded4bc2a77
Curtis Timson <curt@live.co.uk>
2022-03-04 16:49:53 +0000

    ✅ getAccessToken tests (#34)
    * ✅ getAccessToken tests
    
    * add tests for dispatchAction

--- app/functions/webhook/getAccessToken.ts
+++ app/functions/webhook/getAccessToken.ts
@@ -5,9 +5,7 @@ require('dotenv').config();
 export async function getAccessToken(refreshToken: string) {
   return fetch('https://www.strava.com/oauth/token', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       client_id: process.env.STRAVA_CLIENTID,
       client_secret: process.env.STRAVA_CLIENTSECRET,
