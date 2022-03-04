1ea39e8f08a56db07239cf97e34e296b4c531715
Curtis Timson <curt@live.co.uk>
2022-03-04 19:20:04 +0000

    refactor getAuthData

---
+++ app/functions/oauthcallback/getAuthData.ts
@@ -0,0 +1,19 @@
+import { IOAuthTokenResponse } from './IOAuthTokenResponse';
+
+require('dotenv').config();
+
+export async function getAuthData(code: string): Promise<IOAuthTokenResponse> {
+  return fetch('https://www.strava.com/oauth/token', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      client_id: process.env.STRAVA_CLIENTID,
+      client_secret: process.env.STRAVA_CLIENTSECRET,
+      code,
+    }),
+  })
+    .then((res: any) => res.json());
+}
--- app/functions/oauthcallback/oauthcallback.ts
+++ app/functions/oauthcallback/oauthcallback.ts
@@ -1,30 +1,13 @@
 /* eslint-disable no-console */
 import { Handler } from '@netlify/functions';
-import fetch from 'node-fetch';
 import { isAthleteAllowed } from '../../util/isAthleteAllowed';
 import { IAthleteYtd } from '../types/IAthleteYtd';
 import { dispatchAction } from '../util/dispatchAction';
 import { getAthleteYtd } from '../util/getAthleteYtd';
-import { IOAuthTokenResponse } from './IOAuthTokenResponse';
+import { getAuthData } from './getAuthData';
 import { upsertUser } from './upsertUser';
 
 require('dotenv').config();
-
-async function getAuthData(code: string): Promise<IOAuthTokenResponse> {
-  return fetch('https://www.strava.com/oauth/token', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      client_id: process.env.STRAVA_CLIENTID,
-      client_secret: process.env.STRAVA_CLIENTSECRET,
-      code,
-    }),
-  })
-    .then((res: any) => res.json());
-}
 
 export const handler: Handler = async (event: any) => {
   const { code } = event.queryStringParameters;
