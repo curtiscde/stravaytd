ecd46052633507c270109f552072594ea926d07a
Curtis Timson <curt@live.co.uk>
2022-03-05 10:36:08 +0000

    ✅ updateCurrentYtd test

--- github-actions/src/updateCurrentYtd.ts
+++ github-actions/src/updateCurrentYtd.ts
@@ -1,20 +1,3 @@
-import * as fs from 'fs';
-import * as fsp from 'fs/promises';
-
-const updateCurrentYtd = async () => {
-  const athleteId = Number(process.env.npm_config_athleteid);
-  const count = Number(process.env.npm_config_count);
-  const distance = Number(process.env.npm_config_distance);
-  const movingTime = Number(process.env.npm_config_movingtime);
-  const elevationGain = Number(process.env.npm_config_elevationgain);
-
-  const dir = '../data/current-ytd';
-
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
-
-  await fsp.writeFile(`${dir}/athlete${athleteId}.json`, JSON.stringify({
-    athleteId, count, distance, movingTime, elevationGain,
-  }));
-};
+import { updateCurrentYtd } from './util/updateCurrentYtd';
 
 updateCurrentYtd();
---
+++ github-actions/src/util/updateCurrentYtd.test.ts
@@ -0,0 +1,36 @@
+import fsp from 'fs/promises';
+import { updateCurrentYtd } from './updateCurrentYtd';
+
+jest.mock('fs', () => ({ existsSync: () => true }));
+jest.mock('fs/promises', () => ({ writeFile: jest.fn() }));
+
+describe('updateCurrentYtd', () => {
+  const athleteId = 12345;
+  const count = 11;
+  const distance = 12;
+  const movingTime = 13;
+  const elevationGain = 14;
+
+  beforeAll(async () => {
+    process.env.npm_config_athleteid = athleteId.toString();
+    process.env.npm_config_count = count.toString();
+    process.env.npm_config_distance = distance.toString();
+    process.env.npm_config_movingtime = movingTime.toString();
+    process.env.npm_config_elevationgain = elevationGain.toString();
+
+    await updateCurrentYtd();
+  });
+
+  it('writes file', () => {
+    expect(fsp.writeFile).toHaveBeenCalledWith(
+      '../data/current-ytd/athlete12345.json',
+      JSON.stringify({
+        athleteId,
+        count,
+        distance,
+        movingTime,
+        elevationGain,
+      }),
+    );
+  });
+});
