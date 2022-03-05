f68b0130a733dfb60cec396b637c2e1940d0f58a
Curtis Timson <curt@live.co.uk>
2022-03-05 10:47:05 +0000

    ✅ updateCurrentYtd test (#38)
    * ✅ updateCurrentYtd test
    
    * add number type

---
+++ github-actions/src/util/updateCurrentYtd.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+import * as fsp from 'fs/promises';
+
+export const updateCurrentYtd = async () => {
+  const athleteId = Number(process.env.npm_config_athleteid);
+  const count = Number(process.env.npm_config_count);
+  const distance = Number(process.env.npm_config_distance);
+  const movingTime = Number(process.env.npm_config_movingtime);
+  const elevationGain = Number(process.env.npm_config_elevationgain);
+
+  const dir = '../data/current-ytd';
+
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+  await fsp.writeFile(`${dir}/athlete${athleteId}.json`, JSON.stringify({
+    athleteId, count, distance, movingTime, elevationGain,
+  }));
+};
