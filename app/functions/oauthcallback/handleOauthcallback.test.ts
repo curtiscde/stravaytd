6ca95829270e620693dfa88bed76e20fe148c263
Curtis Timson <curt@live.co.uk>
2022-03-07 19:07:33 +0000

    ✅ webhook / oauthcallback tests (#43)

---
+++ app/functions/oauthcallback/handleOauthcallback.test.ts
@@ -0,0 +1,54 @@
+import { handleOauthcallback } from './handleOauthcallback';
+import { IAthleteYtd } from '../types/IAthleteYtd';
+import { authoriseUser } from './authoriseUser';
+import { dispatchAction } from '../util/dispatchAction';
+
+const mockAthleteYtd: IAthleteYtd = {
+  athleteId: 123,
+  movingTime: 1,
+  distance: 2,
+  count: 3,
+  elevationGain: 4,
+};
+
+jest.mock('./authoriseUser', () => ({
+  authoriseUser: jest.fn(() => ({ authorised: true, authData: { accessToken: 'foo', athleteId: 123 } })),
+}));
+
+jest.mock('../util/getAthleteYtd', () => ({
+  getAthleteYtd: jest.fn(() => mockAthleteYtd),
+}));
+
+jest.mock('../util/dispatchAction', () => ({
+  dispatchAction: jest.fn(),
+}));
+
+describe('handleOauthcallback', () => {
+  const event = {
+    queryStringParameters: { code: 'qspcode' },
+  };
+
+  let response: any;
+
+  beforeAll(async () => {
+    response = await handleOauthcallback(event);
+  });
+
+  it('calls authoriseUser', () => {
+    expect(authoriseUser).toHaveBeenCalledWith('qspcode');
+  });
+
+  it('calls dispatchAction', () => {
+    expect(dispatchAction).toHaveBeenCalledWith(mockAthleteYtd);
+  });
+
+  it('returns 200', () => {
+    expect(response).toEqual({
+      statusCode: 200,
+      body: JSON.stringify({
+        verified: true,
+        ytdSet: true,
+      }),
+    });
+  });
+});
