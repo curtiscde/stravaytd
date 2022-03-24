d655a3d60e01c1d051ddea7d0541dc90f9d9670d
curtiscde <curt@live.co.uk>
2022-03-24 19:50:07 +0000

    only commit json file

--- github-actions/src/util/commitAthleteYtd.ts
+++ github-actions/src/util/commitAthleteYtd.ts
@@ -6,7 +6,7 @@ export const commitAthleteYtd = (athleteYtd: IAthleteYtd) => {
   spawn('git', ['diff']);
   spawn('git', ['config', '--global', 'user.email', '"actions@users.noreply.github.com"']);
   spawn('git', ['config', '--global', 'user.name', '"gh action bot"']);
-  spawn('git', ['add', '-A']);
+  spawn('git', ['add', `./data/current-ytd/athlete${athleteYtd.athleteId}.json`]);
   spawn('git', ['commit', '-m', `🏃 Athlete YTD ${athleteYtd.athleteId} (run ${athleteYtd.count})`]);
   spawn('git', ['push']);
 };
