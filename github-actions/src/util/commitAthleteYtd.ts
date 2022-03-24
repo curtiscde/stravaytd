f32db886841221316228f737e3088b1f45c519ab
curtiscde <curt@live.co.uk>
2022-03-24 20:53:40 +0000

    update path

--- github-actions/src/util/commitAthleteYtd.ts
+++ github-actions/src/util/commitAthleteYtd.ts
@@ -8,7 +8,7 @@ export const commitAthleteYtd = (athleteYtd: IAthleteYtd) => {
   spawn('git', ['diff']);
   spawn('git', ['config', '--global', 'user.email', '"actions@users.noreply.github.com"']);
   spawn('git', ['config', '--global', 'user.name', '"gh action bot"']);
-  spawn('git', ['add', `./data/current-ytd/athlete${athleteYtd.athleteId}.json`], { stdio: 'inherit' });
+  spawn('git', ['add', `../data/current-ytd/athlete${athleteYtd.athleteId}.json`], { stdio: 'inherit' });
   spawn('git', ['commit', '-m', commitMessage], { stdio: 'inherit' });
   spawn('git', ['push']);
 };
