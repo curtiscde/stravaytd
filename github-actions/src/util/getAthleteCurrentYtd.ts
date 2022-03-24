37d2542ede8226f6b7e2a24539ec881d2e4fcb59
curtiscde <curt@live.co.uk>
2022-03-24 19:29:59 +0000

    correct filename

--- github-actions/src/util/getAthleteCurrentYtd.ts
+++ github-actions/src/util/getAthleteCurrentYtd.ts
@@ -4,7 +4,7 @@ import { IAthleteYtd } from '../types/IAthleteYtd';
 // eslint-disable-next-line max-len
 export const getAthleteCurrentYtd = (currentYtdPath: string, athleteId: number): IAthleteYtd => {
   try {
-    return JSON.parse(fs.readFileSync(`${currentYtdPath}/${athleteId}.json`, 'utf8'));
+    return JSON.parse(fs.readFileSync(`${currentYtdPath}/athlete${athleteId}.json`, 'utf8'));
   } catch {
     return null;
   }
