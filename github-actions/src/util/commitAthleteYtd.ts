b4f2c7fdc2db60e42ce083c56ae28aa4cb32df7f
curtiscde <curt@live.co.uk>
2022-03-24 20:48:18 +0000

    add inherit

--- github-actions/src/util/commitAthleteYtd.ts
+++ github-actions/src/util/commitAthleteYtd.ts
@@ -2,13 +2,13 @@ import { spawn } from 'child_process';
 import { IAthleteYtd } from '../types/IAthleteYtd';
 
 export const commitAthleteYtd = (athleteYtd: IAthleteYtd) => {
-  const commitMessage = `"🏃 Athlete YTD ${athleteYtd.athleteId} (run ${athleteYtd.count})"`;
+  const commitMessage = `🏃 Athlete YTD ${athleteYtd.athleteId} (run ${athleteYtd.count})`;
 
   spawn('git', ['pull']);
   spawn('git', ['diff']);
   spawn('git', ['config', '--global', 'user.email', '"actions@users.noreply.github.com"']);
   spawn('git', ['config', '--global', 'user.name', '"gh action bot"']);
   spawn('git', ['add', '-A']);
-  spawn('git', ['commit', '-m', commitMessage]);
+  spawn('git', ['commit', '-m', commitMessage], { stdio: 'inherit' });
   spawn('git', ['push']);
 };
