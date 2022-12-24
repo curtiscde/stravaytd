c1be9586094a2b1a7b64c2e941632e5553e0a582
Curtis Timson <curt@live.co.uk>
2022-12-24 16:14:30 +0000

    Refactor getStats (#62)

--- app/util/getStats.test.ts
+++ app/util/getStats.test.ts
@@ -1,5 +1,7 @@
 import { getStats } from './getStats';
 
+const convertToTimestamp = (date: string) => new Date(date).getTime();
+
 jest.mock('fs', () => ({
   readFileSync: () => JSON.stringify({
     athletes: [
@@ -7,10 +9,10 @@ jest.mock('fs', () => ({
         athleteId: 123,
         ytd: [
           {
-            date: 1, distance: 10000, movingTime: 300, elevationGain: 3, count: 4,
+            date: convertToTimestamp('2022-01-01'), distance: 10000, movingTime: 300, elevationGain: 3, count: 4,
           },
           {
-            date: 2, distance: 12000, movingTime: 600, elevationGain: 7, count: 8,
+            date: convertToTimestamp('2022-03-01'), distance: 12000, movingTime: 600, elevationGain: 7, count: 8,
           },
         ],
       },
@@ -18,10 +20,10 @@ jest.mock('fs', () => ({
         athleteId: 456,
         ytd: [
           {
-            date: 1, distance: 20000, movingTime: 300, elevationGain: 4, count: 5,
+            date: convertToTimestamp('2022-01-01'), distance: 20000, movingTime: 300, elevationGain: 4, count: 5,
           },
           {
-            date: 2, distance: 80000, movingTime: 900, elevationGain: 10, count: 11,
+            date: convertToTimestamp('2022-03-01'), distance: 80000, movingTime: 900, elevationGain: 10, count: 11,
           },
         ],
       },
@@ -32,11 +34,13 @@ jest.mock('fs', () => ({
 describe('getStats', () => {
   let stats: any;
 
-  beforeAll(() => {
-    stats = getStats('123:foo,456:bar');
-  });
+  describe('year 2022', () => {
+    beforeAll(() => {
+      stats = getStats({ allowedAthletes: '123:foo,456:bar' });
+    });
 
-  it('returns stats', () => {
-    expect(stats).toMatchSnapshot();
+    it('returns stats', () => {
+      expect(stats).toMatchSnapshot();
+    });
   });
 });
