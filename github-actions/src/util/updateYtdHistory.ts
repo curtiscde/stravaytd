9069548302f098b222cb1f275b7f3ff47dcfeb7d
Curtis Timson <curt@live.co.uk>
2023-01-20 20:11:26 +0000

    Store ytdHistory @ /data (#71)
    * Store ytdHistory @ /data
    
    * copy ytdHistory to /data
    
    Co-authored-by: gh action bot <actions@users.noreply.github.com>

--- github-actions/src/util/updateYtdHistory.ts
+++ github-actions/src/util/updateYtdHistory.ts
@@ -4,16 +4,18 @@ import { generateNewYtd } from './generateNewYtd';
 import { getAthletesCurrentYtd } from './getAthletesCurrentYtd';
 import { getYtdHistory } from './getYtdHistory';
 
-const ytdFileLocation = '../app/data/ytdHistory.json';
+const ytdFileLocationApp = '../app/data/ytdHistory.json';
+const ytdFileLocation = '../data/ytdHistory.json';
 const currentYtdPath = '../data/current-ytd';
 
 export const updateYtdHistory = async () => {
   const now = (new Date()).getTime();
-  const ytd = getYtdHistory(ytdFileLocation);
+  const ytd = getYtdHistory(ytdFileLocationApp);
   const athletesYtd = getAthletesCurrentYtd(currentYtdPath);
   const newYtd = generateNewYtd(ytd, athletesYtd, now);
 
   await fsp.writeFile(ytdFileLocation, JSON.stringify(newYtd));
+  await fsp.writeFile(ytdFileLocationApp, JSON.stringify(newYtd));
 
   core.setOutput('ytdVersion', newYtd.meta.version);
 };
