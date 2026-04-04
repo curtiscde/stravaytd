deeb0b516c4b96d57839a030f6a2694e6acadf3d
Curtis Timson <curt@live.co.uk>
2026-04-04 14:20:11 +0100

    Upgrade: Next v16

--- app/components/Sections.tsx
+++ app/components/Sections.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable  jsx-a11y/anchor-has-content */
 import React from 'react';
 import { Section } from '../types/Section';
 import { formatTooltip } from '../util/formatTooltip';
--- app/functions/webhook/decrypt.ts
+++ app/functions/webhook/decrypt.ts
@@ -5,7 +5,7 @@ export function decrypt(token: string): string {
   const tP2 = textParts.slice(1);
   const iv = Buffer.from(textParts[0], 'hex');
   const encryptedText = Buffer.from(tP2.join(':'), 'hex');
-  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(process.env.CIPHERKEY!), iv);
+  const decipher = crypto.createDecipheriv('aes-256-cbc', process.env.CIPHERKEY!, iv);
   let decrypted = decipher.update(encryptedText);
 
   decrypted = Buffer.concat([decrypted, decipher.final()]);
